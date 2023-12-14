d1921e3a8e0c6df9ce328d6c28b1372f56963957
network-Error <iamimew@gmail.com>
2023-12-14 18:28:03 +0300

    feat: add burger menu

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Logi!');
+// Burger menu
+const bodyBlock = document.querySelector('body');
+const burgerBtn = document.querySelector('.nav__burger-btn');
+const burgerBtnSpan = document.querySelector('.nav__burger-btn span');
+const burgerList = document.querySelector('.nav__burger-wrapper-list');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtnSpan.classList.toggle('_active-burger-btn');
+  burgerList.classList.toggle('_show');
+  bodyBlock.classList.toggle('_block');
+});
